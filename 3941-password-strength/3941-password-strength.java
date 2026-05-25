18226320d51bf753da36e31b5963475376907535
Adityadhanavade <124562622+Adityadhanavade@users.noreply.github.com>
2026-05-25 19:02:23 +0530

    Time: 100 ms (0%), Space: 47.2 MB (100%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int passwordStrength(String password) {
+        
+        int cnt = 0;
+        ArrayList<Character> arrlst = new ArrayList<>();
+        for(int i=0; i<password.length(); i++)
+        {   
+            char ch = password.charAt(i);
+            if(ch >= 'a' && ch <= 'z' && !(arrlst.contains(ch)))
+            {
+                arrlst.add(ch);
+                cnt += 1;
+            }
+            else if(ch >='A' && ch <='Z' && !(arrlst.contains(ch)))
+            {
+                arrlst.add(ch);
+                cnt += 2;
+            }
+            else if(ch >= '0' && ch <= '9' && !(arrlst.contains(ch)))
+            {
+                cnt += 3;
+                arrlst.add(ch);
+            }
+            else if((ch == '!' || ch == '@' || ch == '#' || ch == '$') && !(arrlst.contains(ch)))
+            {
+                cnt += 5;
+                arrlst.add(ch);
+            }
+        }
+        return cnt;
+    }
+}
