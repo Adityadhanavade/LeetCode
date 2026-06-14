e814878196d96952bf1dd78332d542709b3ddea3
Adityadhanavade <124562622+Adityadhanavade@users.noreply.github.com>
2026-06-15 01:32:04 +0530

    Time: 602 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+/*/*here we need to select manager such that they have atlet 5 employee that are 
+reporting  to them*/
+
+select e.name as name from Employee e join Employee e2 on e.id=e2.managerId 
+group by e2.managerId
+having count(e.id)>=5 order by name desc;
